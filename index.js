9b4ff740f8e04bed0de23a84f68e79eee45b3a1d
Lalit Jain <jain.lalit138@gmail.com>
2019-07-13 17:01:41 +0530

    ejs setup

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
+const path = require('path')
 const port = 5000
 
 const app = express();
-
-
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
 app.get('/', (req, res)=>{
+    return res.render('home');
     // res.send(req);
-    res.send("<p>Cool, this is running just fine.</p>")
+    // res.send("<p>Cool, this is running just fine.</p>")
 })
 
 
